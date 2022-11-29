0d637976ac5f7b377afaaef1489e1e88da5f2680
OliveNsu <tasbiul.hasan@northsouth.edu>
2022-11-29 14:54:37 +0600

    display item done

---
+++ api.js
@@ -0,0 +1,67 @@
+//fetch the url
+
+const fetchText = ()=>{
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchItem()}`)
+    .then(res => res.json())
+    .then(data => displayPhone(data.data));
+}
+
+// search item
+
+const searchItem = ()=>{
+    const searchText = document.getElementById('searchText');
+    const value = searchText.value;
+    searchText.value='';
+    return value;
+}
+
+const displayPhone = (data)=>{
+    console.log(data);
+    const displayItem = document.getElementById('displayItem');
+    displayItem.innerHTML = '';
+    const dataslice = data.slice(0,20);
+    dataslice.forEach((item)=>{
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.classList.add('col-md-3');
+        div.classList.add('col-10');
+        div.style.margin = '10px'
+        div.innerHTML = `
+            <img src="${item.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${item.phone_name}</h5>
+              <h6 class="card-text">${item.brand}</h6>
+              <button onclick="fetchDetails('${item.slug}')" class="btn btn-primary">Details</button>
+            </div>
+        `
+        displayItem.appendChild(div);
+    })
+}
+
+const fetchDetails =data=>{
+    console.log(data);
+    const url = `https://openapi.programming-hero.com/api/phone/${data}`;
+    fetch(url).then(res => res.json()).then(data => showdetails(data.data));
+}
+
+const showdetails = (data) =>{
+    console.log(data);
+    const showdetails = document.getElementById('showdetails');
+    showdetails.innerHTML = `
+        <div class="card mx-auto" style="width: 25rem;">
+    <img src="${data.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${data.name}</h5>
+    <h6 class="card-text">${data.brand}</h6>
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">${data.releaseDate}</li>
+        <li class="list-group-item">${data.mainFeatures.storage}</li>
+        <li class="list-group-item">${data.mainFeatures.memory}</li>
+        <li class="list-group-item">${data.mainFeatures.chipSet}</li>
+        <li class="list-group-item">${data.mainFeatures.displaySize}</li>
+    </ul>
+    </div>
+    `;
+
+}
